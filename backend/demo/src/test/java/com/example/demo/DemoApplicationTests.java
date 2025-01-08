3b7dde78e664ebd0ee6042aeb64ff000267a2459
Michael Marconi <michaelziga2@gmail.com>
2025-01-08 00:31:59 -0600

    mvn again

--- backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+import com.example.portfoliotracker.DemoApplication; // Add this import
+
+@SpringBootTest(classes = DemoApplication.class) // Specify the main application class
 class DemoApplicationTests {
 
     @Autowired
