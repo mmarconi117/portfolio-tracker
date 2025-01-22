43ba8a3cc4ed580c2f0d7ac7e705ea8f9d9f8ad3
Michael Marconi <michaelziga2@gmail.com>
2025-01-22 17:41:58 -0600

    redo

--- backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootTest // Tells Spring Boot to search for the main configuration class (DemoApplication) and start the context
+public class DemoApplicationTests {
+
     @Test
     void contextLoads() {
+        // Your test logic here
     }
 }
