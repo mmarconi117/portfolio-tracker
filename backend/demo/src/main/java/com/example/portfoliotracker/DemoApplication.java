1679aeaeafdc98ecf8fe3fbc72e014c01b89bd6c
Michael Marconi <michaelziga2@gmail.com>
2025-01-22 17:27:51 -0600

    jar?

--- backend/demo/src/main/java/com/example/portfoliotracker/DemoApplication.java
+++ backend/demo/src/main/java/com/example/portfoliotracker/DemoApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
+
 
 // File path: /Users/michaelmarconi/portfolio-tracker/backend/demo/src/main/java/com/example/portfoliotracker/service/StockService.java
 // Content:
